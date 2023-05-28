5fac894f1683998025b4112813eb41c6dfe584f9
Davis <dagwood1018@gmail.com>
2023-05-27 20:39:03 -0400

    Updating documentation

--- MNIST/MNIST_precompute.m
+++ MNIST/MNIST_precompute.m
@@ -1,8 +1,8 @@
 % Given the MNIST dataset contained in mnist.mat, this script precomputes
 % a subset of phase masks to use repeatedly for imaging.
 
-mnist= load("mnist/mnist.mat");
-path= "E:\MNIST\"; 
+mnist= load("mnist.mat");
+path= "M:\MNIST\"; 
 
 c= Config.LCR2500('wl', 473);
 slm= SLM( c, 'f', 200);
